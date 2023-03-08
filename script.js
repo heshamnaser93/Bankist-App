1215c4f43b33a8df1402c2466de263d4a7c98f57
heshamnaser93 <heshamnaser93@icloud.com>
2023-03-08 18:01:57 +0300

    v0.2.6

--- script.js
+++ script.js
@@ -396,7 +396,29 @@ console.log(Math.max(5, 8, 7, 2, 44, 65));
 console.log(Math.max(5, 8, 7, 2, 44, '65'));
 console.log(Math.min(5, 8, 7, 2, 44, 65));
 console.log(Math.PI * Number.parseFloat('10px') ** 2);
-console.log(Math.trunc(Math.random() * 10) + 1);
+console.log(Math.trunc(Math.random() * 6) + 1); //output will be random between 1 and 6
+
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min - 1) + (min + 1)); //(min-1) and (min+1) because i want the result in between them not from min to max
+
+console.log(randomInt(5, 7));
+
+//Rounding Integers
+console.log(Math.trunc(23.3)); // 23
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+console.log(Math.ceil(23.4)); // 24
+console.log(Math.floor(23.9)); // 23
+console.log(Math.floor('23.3')); // 23
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24
+
+//Rounding Decimals
+console.log((2.7).toFixed(0)); // 3 but string
+console.log((2.7).toFixed(3)); // 2.700 but string
+console.log((2.345).toFixed(2)); // 2.35 but string
+console.log(+(2.345).toFixed(2)); // 2.35 => here type is number
+
 ///////////////////////////////////
 ////End Lectures/////////////////////
 
